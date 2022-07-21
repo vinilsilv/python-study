68896ff8bc39e6ef3f0152fe29038ea5933e41d7
Vincius Silva <vinilrsilv@gmail.com>
2022-07-21 09:00:44 -0300

    feat: 'Madlibs' game added

---
+++ madlibs.py
@@ -0,0 +1,18 @@
+# string concatenarion
+# "subscribe to ____"
+# youtuber = "Me" # some string variable
+
+# print("subscribe to " + youtuber)
+# print("subscribe to {}".format(youtuber))
+# print(f"subscribe to {youtuber}")
+
+
+adj = input("Adjective: ")
+verb1 = input("Verb 1: ")
+verb2 = input("Verb 2: ")
+famous_person = input("Famous Person Name: ")
+
+madlib = f"Computer programming is so {adj}! It makes me excited all the time because \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
