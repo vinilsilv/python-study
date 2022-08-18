054c84aa9833c82891b18bdf4096079584d17087
Vincius Silva <vinilrsilv@gmail.com>
2022-08-17 21:43:14 -0300

    Messing around with strings

---
+++ strings.py
@@ -0,0 +1,18 @@
+# Strings
+text = 'Hello, Friend.'
+print(text)
+
+# Multiline strings
+tripleQuotes = """Tripple
+quotes
+string"""
+
+lineBreak = "This \nhas \nmultiple \nlines"
+
+multipleLines = "Déjà vu. " * 10
+
+print(multipleLines)
+
+
+# Concatenation
+print(text + lineBreak)
